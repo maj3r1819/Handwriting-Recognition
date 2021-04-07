a48c06c77a00d498e34b2c8d23b6b7a013944637
Sagar <major1819@gmail.com>
2021-04-07 12:26:11 +0530

    New Classifiers tried and added

--- Handwriting_Recognition_KNN.py
+++ Handwriting_Recognition_KNN.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as pt
 import pandas as pd
 import cv2
+
 
 # Importing the dataset
 dataset = pd.read_csv('train.csv')
@@ -12,34 +12,20 @@ X_test = dataset.iloc[21000:, 1:].values
 y_test = dataset.iloc[21000:, 0].values
 
 
-#Training the dataset
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(random_state=0)
-classifier.fit(X_train, y_train)
 
+# Training the K-NN model on the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 #print(cm)
-print("Accuracy of the Random Forest Model :" ,accuracy_score(y_test, y_pred)*100 ,"%")
-
-
-
-#Testing the test set!!!
-"""
-d =X_test[100]
-d.shape = (28,28)
-print(X_test[100])
-print(d)
-pt.imshow(d, cmap='gray')
-print(classifier.predict([X_test[100]]))
-"""
-
+print("Accuracy of the KNN Model :" ,accuracy_score(y_test, y_pred) *100 ,"%")
 
 #Testing with 'img.png'
 
--- Handwriting_Recogniton_Logistic_Regression.py
+++ Handwriting_Recogniton_Logistic_Regression.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as pt
 import pandas as pd
 import cv2
+
 
 # Importing the dataset
 dataset = pd.read_csv('train.csv')
@@ -12,34 +12,19 @@ X_test = dataset.iloc[21000:, 1:].values
 y_test = dataset.iloc[21000:, 0].values
 
 
-#Training the dataset
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(random_state=0)
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
-
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 #print(cm)
-print("Accuracy of the Random Forest Model :" ,accuracy_score(y_test, y_pred)*100 ,"%")
-
-
-
-#Testing the test set!!!
-"""
-d =X_test[100]
-d.shape = (28,28)
-print(X_test[100])
-print(d)
-pt.imshow(d, cmap='gray')
-print(classifier.predict([X_test[100]]))
-"""
-
+print("Accuracy of the Logistic Model :" ,accuracy_score(y_test, y_pred) *100 ,"%")
 
 #Testing with 'img.png'
 
